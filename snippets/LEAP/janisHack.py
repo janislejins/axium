4dd29390a75459f6e3a97ce4bd5480720282a29d
Janis Lejins <janis@janislejins.com>
2015-12-22 23:23:39 +1100

    added rudimental pedestrian tracking and cluster analysis on said pedestrians - jped.py

--- snippets/LEAP/janisHack.py
+++ snippets/LEAP/janisHack.py
@@ -8,9 +8,11 @@ import Leap, sys, thread, time
 import threading
 from random import randint
 from requests_futures.sessions import FuturesSession
+import struct
 #global variables
 finger_x = 0
 finger_y = 0
+lightsON = True
 # X & Y for the channel
 Channel = ['0','0','0','0','0','0','0','0','0','0','0','0','0']
 def translate(value, leftMin, leftMax, rightMin, rightMax):
@@ -40,6 +42,13 @@ class SampleListener(Leap.Listener):
     def on_frame(self, controller):
         # Get the most recent frame and report some basic information
         frame = controller.frame()
+        if frame.hands.is_empty:
+            #Process hands...
+            global lightsON
+            lightsON = False
+        else:
+            global lightsON
+            lightsON = True
         # Get hands
         for hand in frame.hands:
             #find the left hand
@@ -60,6 +69,8 @@ class SampleListener(Leap.Listener):
                         #the bone direction is mapped relative to the hand so the light is tip of finger...
                         finger_x = translate(bone.direction[0], -1.0, 1.0, 0, 66)
                         finger_y = translate(bone.direction[1], -1.0, 1.0, 100, 10)
+
+
 def SendXY():
     # make sure variables aren't NoneTypes and if they are change that
     # if finger_x is None:
@@ -71,7 +82,12 @@ def SendXY():
     ysend = str(int(finger_y))
     Channel[0] = xsend
     Channel[2] = ysend
-    Channel[5] = '255'
+    
+    if lightsON is True:
+        Channel[5] = '255'
+    else:
+        Channel[5] = '0'
+
     Channel[7] = '255'
     Channel[8] = '0'#str(randint(0,255))
     Channel[9] = '0' #str(randint(0,255))
@@ -84,17 +100,18 @@ def SendXY():
     t.daemon = True #this shit kills the thread when the program finishes..
     t.start()
 
-
-# Create a sample listener and controller
-listener = SampleListener()
-controller = Leap.Controller()
-# create future session for async requests
-session = FuturesSession()
-# Have the sample listener receive events from the controller
-controller.add_listener(listener)
-#send the coordinates async
-SendXY()
-time.sleep(240)
+while True:
+    # Create a sample listener and controller
+    listener = SampleListener()
+    controller = Leap.Controller()
+    # create future session for async requests
+    session = FuturesSession()
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+    #send the coordinates async
+    SendXY()
+    infinity = float("inf")
+# time.sleep(10000000)
 # Remove the sample listener when done
 controller.remove_listener(listener)
 # quit the program, kills all threads - if they are set to Thread.daemon = True
---
+++ snippets/pedestriantracking/janisped/jped.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+import sys
+import math
+
+cascPath = 'hogcascade_pedestrians.xml'
+pedCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+def sequencers():
+    pass
+
+sequencers()
+
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    screen = np.zeros((frame.shape[0],frame.shape[1],3), np.uint8)
+    # convert to BW
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    people = pedCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    # an empty list which will fill up with location of people
+    points = []
+    # for each person you see do something
+    for i, (x, y, w, h) in enumerate(people):
+        # draw a rectangle round them
+        # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # draw a dot in the middle of them...
+        cv2.circle(screen,(x+w/2,y+h/2), 5, (255,255,255), -1)
+        # add their location to the list
+        points.insert(i, (x+w/2,y+h/2))
+    # if the list is empty
+    if not points:
+        # print "no people"
+        pass
+    else:
+        # peoples locations
+        #print points
+        x = [p[0] for p in points]
+        y = [p[1] for p in points]
+        #find centre of group of people
+        centroid = (sum(x) / len(points), sum(y) / len(points))
+        print centroid
+        # find the person furthest from the centre
+        furthest = max(points, key=lambda point: math.hypot(point[0]-centroid[0], point[1]-centroid[1]))
+        #find the distance between the centroid and furthest person.
+        radius = math.hypot(centroid[0] - furthest[0], centroid[1] - furthest[1])
+        # draw a circle to the limit of cluster of people by the person who is furthest away
+        cv2.circle(screen, (centroid[0],centroid[1]), int(radius), (0,0,255), 1)
+    # Display the resulting frame
+    cv2.imshow('Tracker', screen)
+    # if i press q leave the infinite loop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
